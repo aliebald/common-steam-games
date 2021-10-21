e5c8a5bd6af80468d9580f071bb5de8a6e46c162
Alexander Liebald <alexander.liebald@gmail.com>
2021-10-21 18:23:22 +0200

    Added github icon/link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,18 @@ import "../styles/footer.css"
 export default function Footer() {
   return (
     <div className="footer">
-      <div className="left">
+      <div className="f-left">
         This project is not affiliated with Valve or Steam
       </div>
+      <a
+        className="f-right"
+        href="https://github.com/aliebald/common-steam-games"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="GitHub Repository"
+      >
+        <img src="/github.png" alt="github" height="20px" width="20px" />
+      </a>
 
     </div>
   )
