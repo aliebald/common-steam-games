3ec384f4b1ce9309b52307fae3bfb9f7cd4c0a47
Alexander Liebald <alexander.liebald@gmail.com>
2021-10-28 12:05:22 +0200

    Reworked Invite Component

--- src/components/Invite.tsx
+++ src/components/Invite.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import packageJSON from "../../package.json"
 import "../styles/invite.css"
+import Button from './Button';
 
 export default function Invite(props: {
   sessionId: string,
   className?: string
 }) {
   const invite = `${window.location.origin}${packageJSON.subUrl}/join?sessionId=${encodeURIComponent(props.sessionId)}`;
+  const copyInvite = () => {
+    navigator.clipboard.writeText(invite);
+  }
+
   return (
     <div className={`inviteBox ${props.className ?? ""}`}>
-      Invite&nbsp;Link:&nbsp;
-      <a href={invite} target="_blank" rel="noopener noreferrer">
-        {props.sessionId}
-      </a>
+      <label>Invite your friends
+        <input className="invite-input" type="text" value={invite} />
+      </label>
+      <Button text="Copy Invite" onClick={copyInvite} />
+      <Button text="Open friends list" />
     </div>
   )
 }
